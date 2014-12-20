d081cfc92dc987646505dd7718920565e7e334f2
XiaoGui <higugu88@hotmail.com>
2014-12-20 12:08:30 +0800

    跑酷場景 > 玩家自動向前. 空白鍵跳躍

---
+++ Assets/Script/player.js
@@ -0,0 +1,15 @@
+﻿#pragma strict
+
+var speed:float=5.0f;
+var jumpforce:float=500.0f;
+
+
+function Update () {
+
+	rigidbody2D.velocity=new Vector2(speed, rigidbody2D.velocity.y);
+	if(Input.GetButtonDown("Jump")){
+		rigidbody2D.AddForce(new Vector2(0, jumpforce));
+		
+	}
+	
+}
