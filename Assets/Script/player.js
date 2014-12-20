6652cd935bfc9850f4362658a87622506a2def82
XiaoGui <higugu88@hotmail.com>
2014-12-20 15:00:00 +0800

    跑酷場景 > 鏡頭跟隨.禁止連跳.死穴

--- Assets/Script/player.js
+++ Assets/Script/player.js
@@ -2,14 +2,30 @@
 
 var speed:float=5.0f;
 var jumpforce:float=500.0f;
+var isJumping : boolean = false;
 
 
 function Update () {
 
 	rigidbody2D.velocity=new Vector2(speed, rigidbody2D.velocity.y);
-	if(Input.GetButtonDown("Jump")){
+	if(Input.GetButtonDown("Jump")&&isJumping==false){
 		rigidbody2D.AddForce(new Vector2(0, jumpforce));
 		
 	}
+}
+
+function OnTriggerEnter2D (other : Collider2D) {
+	if(other.tag == "ground") {
+		isJumping = false;
+		}
+	else if(other.tag=="Deadpit"){
+	transform.position=GameObject.Find("Startpoint").transform.position;
 	
+	}
+}
+
+function OnTriggerExit2D (other : Collider2D) {
+	if(other.tag == "ground") {
+		isJumping = true;
+	}
 }
